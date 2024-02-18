90e878a1054a393c2dc781ae2c3645d86cc887ce
VesNa <nat.veselowa2010@yandex.ru>
2024-02-18 19:29:19 +0000

    added code for writing README and initializing the program

--- index.js
+++ index.js
@@ -8,57 +8,66 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        massage: 'What is the title of your project?',
+        message: 'What is the title of your project?',
     },
     {
         type: 'input',
         name: 'description',
-        massage: 'Add description for your project',
+        message: 'Add description for your project',
     },
     {
         type: 'input',
         name: 'installation',
-        massage: 'What is the installation process?',
+        message: 'What is the installation process?',
     },
     {
         type: 'input',
         name: 'usage',
-        massage: 'How will this project be used?',
+        message: 'How will this project be used?',        
     },
     {
         type: 'list',
         name: 'license',
-        massage: 'What license is required with this project?',
+        message: 'What license is required with this project?',
+        choices: ['MIT License', 'GNU General Public License', 'Apache License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense', 'N/A'],
     },
     {
         type: 'input',
         name: 'contribution',
-        massage: 'How can someone contribute to this project?',
+        message: 'How can someone contribute to this project?',
     },
     {
         type: 'input',
         name: 'tests',
-        massage: 'Provide instruction for testing your application?',
+        message: 'Provide instruction for testing your application?',
     },
     {
         type: 'input',
         name: 'GitHub',
-        massage: 'What is your GitHub username?',
+        message: 'What is your GitHub username?',
     },
     {
         type: 'input',
         name: 'email',
-        massage: 'What is your email address?',
+        message: 'What is your email address?',
     },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) { //generateMarkdown()
+function writeToFile(fileName, data) {
+    const readme = generateMarkdown(data);
+    fs.writeFile(`./${fileName}.md`, readme, (error) =>
+        error ? console.error('Error writing README file: ', error) : console.log('README file generated successfully')
+    )    
 }
 
 // function to initialize program
 function init() {
-//inquirer.prompt()
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            writeToFile(answers.title, answers);
+        })
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,45 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return 
+  `# ${data.title}
+  
+  ## Description
 
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${data.license}
+
+  ## Contribution
+
+  ${data.contribution}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  [${data.GitHub}](https://github.com/${data.GitHub})
+  [${data.email}](mailto:${data.email})
 `;
 }
 
