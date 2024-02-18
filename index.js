5d6bc22802c1a5d8827bb6e101ecb8171c6052bb
VesNa <nat.veselowa2010@yandex.ru>
2024-02-18 17:34:48 +0000

    added questions array

--- index.js
+++ index.js
@@ -6,17 +6,59 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
     {
-        
-    }
+        type: 'input',
+        name: 'title',
+        massage: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        massage: 'Add description for your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        massage: 'What is the installation process?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        massage: 'How will this project be used?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        massage: 'What license is required with this project?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        massage: 'How can someone contribute to this project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        massage: 'Provide instruction for testing your application?',
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        massage: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        massage: 'What is your email address?',
+    },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(fileName, data) { //generateMarkdown()
 }
 
 // function to initialize program
 function init() {
-
+//inquirer.prompt()
 }
 
 // function call to initialize program
