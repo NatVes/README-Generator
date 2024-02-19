c287f2c081899bfe30d05dffbacc9876805a46d2
VesNa <nat.veselowa2010@yandex.ru>
2024-02-19 13:28:42 +0000

    added code for license badges

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'What license is required with this project?',
-        choices: ['MIT License', 'GNU General Public License', 'Apache License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense', 'N/A'],
+        choices: ['Apache 2.0 License', 'Boost Software License 1.0', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU General Public License v3.0', 'GNU General Public License v2.0', 'GNU Affero General Public License v3.0', 'GNU Lesser General Public License v3.0', 'MIT License', 'Mozilla Public License 2.0', 'The Unlicense', 'N/A'],
     },
     {
         type: 'input',
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,69 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  let  licenseBadgePath = '';
+  let licenseBadgeLink = '';
+
+  switch (data.license) {
+    case 'Apache License':
+      licenseBadgePath = 'Apache_2.0-blue';
+      licenseBadgeLink = 'https://opensource.org/licenses/Apache-2.0';
+      break;
+    case 'Boost Software License 1.0':
+      licenseBadgePath = 'Boost_1.0-lightblue';
+      licenseBadgeLink = 'https://www.boost.org/LICENSE_1_0.txt';
+      break;
+    case 'BSD 2-Clause "Simplified" License':
+      licenseBadgePath = 'BSD_2--Clause-orange';
+      licenseBadgeLink = 'https://opensource.org/licenses/BSD-2-Clause';
+      break;
+    case 'BSD 3-Clause "New" or "Revised" License':
+      licenseBadgePath = 'BSD_3--Clause-orange';
+      licenseBadgeLink = 'https://opensource.org/licenses/BSD-3-Clause';
+      break;
+    case 'Creative Commons Zero v1.0 Universal':
+      licenseBadgePath = 'CC0_1.0-lightgrey';
+      licenseBadgeLink = 'http://creativecommons.org/publicdomain/zero/1.0/';
+      break;
+    case 'Eclipse Public License 2.0':
+      licenseBadgePath = 'EPL_2.0-red';
+      licenseBadgeLink = 'https://opensource.org/licenses/EPL-2.0';
+      break;
+    case 'GNU General Public License v3.0':
+      licenseBadgePath = 'GPLv3-blue';
+      licenseBadgeLink = 'https://www.gnu.org/licenses/gpl-3.0';
+      break;
+    case 'GNU General Public License v2.0':
+      licenseBadgePath = 'GPL_v2-blue';
+      licenseBadgeLink = 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html';
+      break;
+    case 'GNU Affero General Public License v3.0':
+      licenseBadgePath = 'AGPL_v3-blue';
+      licenseBadgeLink = 'https://www.gnu.org/licenses/agpl-3.0';
+      break;
+    case 'GNU Lesser General Public License v3.0':
+      licenseBadgePath = 'LGPL_v3-blue';
+      licenseBadgeLink = 'https://www.gnu.org/licenses/lgpl-3.0';
+      break;
+    case 'MIT License':
+      licenseBadgePath = 'MIT-yellow';
+      licenseBadgeLink = 'https://opensource.org/licenses/MIT';
+      break;
+    case 'Mozilla Public License 2.0':
+      licenseBadgePath = 'MPL_2.0-brightgreen';
+      licenseBadgeLink = 'https://opensource.org/licenses/MPL-2.0';
+      break;
+    case 'The Unlicense':
+      licenseBadgePath = 'Unlicense-blue';
+      licenseBadgeLink = 'http://unlicense.org/';
+      break;
+    default:
+      licenseBadgePath = 'license-blue';
+      licenseBadgeLink = 'Unknown';
+  }
+
   return `# ${data.title}
+
+  [![License: ${data.license}](https://img.shields.io/badge/License-${licenseBadgePath}.svg)](${licenseBadgeLink})
   
   ## Description
 
@@ -38,7 +101,7 @@ function generateMarkdown(data) {
   ## Questions
 
   [${data.GitHub}](https://github.com/${data.GitHub})
-  
+
   [${data.email}](mailto:${data.email})`;
 }
 
